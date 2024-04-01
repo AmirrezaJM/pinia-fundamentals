e80cc309e88684a1a8996e9e682ad783ebb27b39
AmirrezaJM <amirreza.jolani@yahoo.com>
2024-04-01 15:29:24 +0200

    lecture5-start

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,7 @@
 import { useTodoListStore } from '@/stores/todoList'
 import { storeToRefs } from 'pinia'
 
-const store = useTodoListStore();
+const store = useTodoListStore()
 
 // storeToRefs lets todoList keep reactivity:
 const { todoList } = storeToRefs(store)
@@ -13,8 +13,8 @@ const { toggleCompleted } = store
 </script>
 
 <template>
-  <div v-for="todo in todoList" :key="todo.id" class="list">
-    <div class="item">
+  <div v-for="todo in todoList" :key="todo.id" class="item">
+    <div class="content">
       <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
       <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
     </div>
